9bb83d0e7d5ef748658e46df46999846aae25ebd
Mohamed <Mohamed@DESKTOP-T4MASGO>
2025-04-16 18:04:27 +0200

    New

--- infrastructures/Repository/Repository.cs
+++ infrastructures/Repository/Repository.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using RestaurantManagementSystem.Repository.IRepository;
 using System;
 using System.Collections.Generic;
